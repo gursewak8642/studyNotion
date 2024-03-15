788741cbfbc8366ce196104821bd907eefe3c453
gursewak8642 <gursewak8642@gmail.com>
2024-03-15 23:34:24 +0530

    code completed

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = ({ setIsLoggedIn }) => {
+  return (
+    <div className="flex justify-center items-center text-white text-3xl w-screen h-screen">
+      Home
+    </div>
+  );
+};
+export default Home;
